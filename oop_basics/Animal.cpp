37a815269e59cfaff4e551176a8dcc1a42fa4b87
Ivan Slaev <islaev@lin-0225.ent.mm-sol.com>
2017-02-21 17:52:11 +0200

    first commit

---
+++ oop_basics/Animal.cpp
@@ -0,0 +1,16 @@
+namespace AnimalSystem {
+	#include "cpplinq.hpp"
+
+	public class Animal : public ISound {
+		protected:
+			int age;
+			int name;
+			SexEnum sex;
+
+		public:
+			virtual ISoundLike;
+			template <typename T>
+			static string GetAvegareAgeByType(Animal[] animals) {
+				int sum = 
+	}
+}
